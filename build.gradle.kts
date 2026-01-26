cb9b2af172fa44f97a61dfe1519e101c3f4c561e
Nikita Volzhin <vlzh.nik@mail.ru>
2026-01-26 21:40:32 +0500

    Update Loki4j dependency and add console logging appender.

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.cloud:spring-cloud-starter-gateway")
     //implementation("org.springframework.boot:spring-boot-starter-security")
-    implementation("com.github.loki4j:loki-logback-appender:2.0.3")
+    implementation("com.github.loki4j:loki-logback-appender:1.5.0")
     implementation("io.jsonwebtoken:jjwt-api:0.11.5")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
